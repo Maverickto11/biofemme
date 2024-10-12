f0268c895f9405a09d7016ac834802f89bda568c
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-10-12 08:25:52 -0500

    footer

--- src/app/Home/contenido/cards.component.ts
+++ src/app/Home/contenido/cards.component.ts
@@ -16,4 +16,37 @@ export class CardsComponent {
     { nombre: 'PROAGE', imagen: '/assets/img/Categorías/categoria img 4.jpg', imagenHover: '/assets/img/Categorías/categoria img hover 4.jpg' },
     { nombre: 'ACNÉ CONTROL', imagen: '/assets/img/Categorías/categoria img 5.jpg' , imagenHover: '/assets/img/Categorías/categoria img hover 5.jpg' }
   ];
+
+  productos = [
+    {
+      imagen: '/assets/img/Producto/Producto1.jpg',
+      titulo: 'LIFTING SERUM',
+      tituloSub: 'Sérum Tensor',
+      descripcion: 'Proage | Todo tipo de piel',
+      precio: '$21.57'
+    },
+    {
+      imagen: '/assets/img/Producto/Producto2.jpg',
+      titulo: 'SUNSCREEN 50+',
+      tituloSub: 'Bloqueador Solar',
+      descripcion: 'Protección | Todo tipo de piel',
+      precio: '$24.64'
+    },
+    {
+      imagen: '/assets/img/Producto/Producto3.jpg',
+      titulo: 'BIO BEAUTY OIL',
+      tituloSub: 'Aceite Seco Hidratante',
+      descripcion: 'Hidratación | Todo tipo de piel',
+      precio: '$16.42'
+    },
+    {
+      imagen: '/assets/img/Producto/Producto4.jpg',
+      titulo: 'BIOFEMME MINI KIT',
+      tituloSub: 'Mini Sizes',
+      descripcion: 'Sets | Piel mixta a grasa',
+      precio: '$20.70 ',
+      precioAnterior: '$23.00'
+    },
+    // Agrega más productos según sea necesario
+  ];
 }
